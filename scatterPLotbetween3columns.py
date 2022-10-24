6b6f532fa3254a9d0f035cc8c537ef2ea3effa06
Mohammad Al Jadallah <70474875+mohammadjadallah@users.noreply.github.com>
2022-10-24 16:37:10 +0300

    Initial Commit
    scatter plot between three variables in matplotlib

--- scatterPLotbetween3columns.py
+++ scatterPLotbetween3columns.py
@@ -6,7 +6,7 @@ print(data)
 # data.plot(kind='scatter', y='Lab', x='established in', c='num of device')
 
 # or this way
-plt.scatter(x='Lab', y='established in', c='num of device', data=data)
+plt.scatter(x='Lab', y='num of device', c='established in', data=data)
 # plt.colorbar().ax.set_title("num of device")  # for title of colorbar
 # customize the chart
 color_bar = plt.colorbar()
@@ -14,7 +14,6 @@ color_bar.set_label("num of device", fontsize=10)  # label of colorbar, you can 
 plt.xlabel('Lab', fontsize=10)
 plt.ylabel('established in', fontsize=10)
 plt.show()
-
 
 
 # Created By Mohammad Al Jadallah, developerx2
